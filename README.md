34f8b878daeb6f7c416e5c7930df344d7e4bf78f
CTK WARRIOR <>
2021-04-03 11:35:20 +0000

    fixed the bugs and added regex

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+![](https://cdn.discordapp.com/attachments/675669552796925987/827854886888538132/unknown.png)
+
 # Not-Quite-Nitro-Using-Discord.JS
+
+* Now you are able to use other server emoji, it wont work if bot will not have access to other server.
+
+* Fixed the bugs
+
+* Regex Based Message checking.
+
+* now it can also be host on glitch.
+
+> Join server, if you like my work :) https://withwin.in//dbd
--- index.js
+++ index.js
@@ -1,82 +1,61 @@
 const discord = require("discord.js");
 const client = new discord.Client();
+const { token } = require("./config.json")
 
 client.on("ready", () => {
-    console.log("[--------------------- R E A D Y ---------------------]");
-    client.user.setActivity("I am NQN 2");
+  console.log("[--------------------- R E A D Y ---------------------]");
+  client.user.setActivity("I am NQN 2");
 })
 
 client.on("message", async (message) => {
-    if(message.author.bot) return;
-    let substringArray = get_substrings_between(message.content, ":", ":");
-    let msg = message.content;
-    if(!substringArray.length) return;
+  if (message.author.bot) return;
+  let msg = message.content;
 
-    substringArray.forEach(m => {
-        let emoji = client.emojis.cache.find(x => x.name === m);
-        var replace = `:${m}:`;
-        var rexreplace = new RegExp(replace, 'g');
+  let emojis = msg.match(/(?<=:)([^:\s]+)(?=:)/g)
+  if (!emojis) return;
+  emojis.forEach(m => {
+    let emoji = client.emojis.cache.find(x => x.name === m)
+    if (!emoji) return;
+    let temp = emoji.toString()
+    if (new RegExp(temp, "g").test(msg)) msg = msg.replace(new RegExp(temp, "g"), emoji.toString())
+    else msg = msg.replace(new RegExp(":" + m + ":", "g"), emoji.toString());
+  })
 
-        if(emoji && !msg.split(" ").find(x => x === emoji.toString()) && !msg.includes(`<a${replace}${emoji.id}>`)) msg = msg.replace(rexreplace, emoji.toString());
-    })
-    
+  if (msg === message.content) return;
 
-    if(msg === message.content) return;
+  let webhook = await message.channel.fetchWebhooks();
+  let number = randomNumber(1, 2);
+  webhook = webhook.find(x => x.name === "NQN" + number);
 
-    let webhook = await message.channel.fetchWebhooks();
-    webhook = webhook.find(x => x.name === "NQN2");
+  if (!webhook) {
+    webhook = await message.channel.createWebhook(`NQN` + number, {
+      avatar: client.user.displayAvatarURL({ dynamic: true })
+    });
+  }
 
-    if(!webhook) {
-        webhook = await message.channel.createWebhook(`NQN2`, {
-            avatar: client.user.displayAvatarURL({dynamic: true})
-        });
-    }
+  await webhook.edit({
+    name: message.member.nickname ? message.member.nickname : message.author.username,
+    avatar: message.author.displayAvatarURL({ dynamic: true })
+  })
 
-    await webhook.edit({
-        name: message.member.nickname ? message.member.nickname : message.author.username,
-        avatar: message.author.displayAvatarURL({dynamic: true})
-    })
+  message.delete().catch(err => { })
+  webhook.send(msg).catch(err => { })
 
-    message.delete().catch(m => {})
+  await webhook.edit({
+    name: `NQN` + number,
+    avatar: client.user.displayAvatarURL({ dynamic: true })
+  })
 
-    webhook.send(msg).catch( m => {});
 
-    await webhook.edit({
-        name: `NQN2`,
-        avatar: client.user.displayAvatarURL({dynamic:true})
-    })
-
- 
 })
 
 
 
-client.login("NzI1MDI2NDg3NjE0MzA4NDMz.XvIviA.cyfOXZn987pX9ZRNO9lDjwSRPGQ");
-
-
+client.login(token);
 //--------------------------------------------------- F U N C T I O N S --------------------------------------
 
-function get_substrings_between(str, startDelimiter, endDelimiter) {
-    var contents = [];
-    var startDelimiterLength = startDelimiter.length;
-    var endDelimiterLength = endDelimiter.length;
-    var startFrom = contentStart = contentEnd = 0;
-  
-    while (false !== (contentStart = strpos(str, startDelimiter, startFrom))) {
-      contentStart += startDelimiterLength;
-      contentEnd = strpos(str, endDelimiter, contentStart);
-      if (false === contentEnd) {
-        break;
-      }
-      contents.push(str.substr(contentStart, contentEnd - contentStart));
-      startFrom = contentEnd + endDelimiterLength;
-    }
-  
-    return contents;
-  }
-  
-  
-  function strpos(haystack, needle, offset) {
-    var i = (haystack + '').indexOf(needle, (offset || 0));
-    return i === -1 ? false : i;
-  }
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
